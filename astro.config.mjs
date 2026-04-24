3f82c93980845522a2598a07f3ac14e119903abb
verasanna <sycheva.testenv@gmail.com>
2026-04-24 16:15:23 +0200

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://verasanna.github.io',
+	base: '/litlunches',
 	integrations: [
 		starlight({
 			title: 'Litlunches',
