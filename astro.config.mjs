e1c18ef53ef4772a9c4b9dd9ad9cb338cd9608d6
verasanna <sycheva.testenv@gmail.com>
2026-04-27 09:25:58 +0200

    robots, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://verasanna.github.io',
+	site: 'https://litlunches.space/',
 	integrations: [
 		starlight({
 			title: 'Litlunches',
