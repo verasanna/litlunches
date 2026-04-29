0a3905198c4973b2f83c36f0877973be781b5324
verasanna <sycheva.testenv@gmail.com>
2026-04-29 10:52:26 +0200

    trailing slash fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://litlunches.space/',
 	integrations: [
